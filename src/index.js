dc47568ae5a7afa5fc0df897fbe4989bae899fa1
Alicia Buelow <alicia@buelow.com.au>
2026-01-16 20:08:25 +0800

    Added search function and basic weather refresh using API, minor CSS tweaks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function searchForCity(event) {
+function refreshWeather(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let countryElement = document.querySelector("#country");
+  countryElement.innerHTML = response.data.country;
+}
+
+function searchCity(city) {
+  let apiKey = "04dbc8004716437tab5bc0bfo1baf277";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchEngine = document.querySelector("#search-form");
-searchEngine.addEventListener("submit", searchForCity);
+searchEngine.addEventListener("submit", submitSearch);
+
+searchCity("Perth");
