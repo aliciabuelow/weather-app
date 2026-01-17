04437959a7489a5421e881694314e0a5a7e4e3a2
Alicia Buelow <alicia@buelow.com.au>
2026-01-17 11:27:53 +0800

    Formatted and injected live date and time from API
    Experiencing a bug with live 'minutes'.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,37 @@ function refreshWeather(response) {
   let weatherConditionText = document.querySelector("#weather-condition");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateTimeElement = document.querySelector("#date-time");
+  let date = new Date(response.data.time * 1000);
 
+  dateTimeElement.innerHTML = formattedDate(date);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.temperature.humidity;
   weatherConditionText.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   countryElement.innerHTML = response.data.country;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  console.log(response.data);
+}
+
+function formattedDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0{${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
