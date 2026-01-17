e1b396286b0e050929df9b0fb2076101ea6999b0
Alicia Buelow <alicia@buelow.com.au>
2026-01-17 12:06:12 +0800

    Adjusted flexbox

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind");
   let dateTimeElement = document.querySelector("#date-time");
   let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#current-temperature-icon");
 
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
   dateTimeElement.innerHTML = formattedDate(date);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.temperature.humidity;
@@ -32,7 +34,7 @@ function formattedDate(date) {
   let minutes = date.getMinutes();
 
   if (minutes < 10) {
-    minutes = `0{${minutes}`;
+    minutes = `0${minutes}`;
   }
 
   return `${day} ${hours}:${minutes}`;
